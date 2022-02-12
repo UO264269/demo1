15e13e850f73db2ce6f4993b9ea9a66e3b3110dd
Álvaro <lvaro@PC_Nox>
2022-02-12 15:09:53 +0100

    Conflicto solucionado

--- src/demo/Demo.java
+++ src/demo/Demo.java
@@ -4,7 +4,7 @@ public class Demo {
 	public static void main(String[] args) {
 		int v = 1;
 		System.out.printf("Prueba git %d", v);
-		System.out.print("Linea 2 con conflicto localle");
+		System.out.print("Linea 2 con conflicto local");
 		System.out.print("Linea 3");
 		System.out.print("Linea en nueva rama");
 	}
